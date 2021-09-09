9a71e144e5f1213ca65843ed6f77249bf85b6f03
Benjamin-von-Barner-Altenburg <75121300+Benjamin-von-Barner-Altenburg@users.noreply.github.com>
2021-09-09 22:58:51 +0200

    Ex5 (#2)
    * EX4 Done
    
    * implemented 75% error handling.
    
    * Implemented error handling.

--- Lecture01/Program.cs
+++ Lecture01/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace Lecture01
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+namespace Lecture01  
+{  
+    public class Program   
+    {  
+       public static bool isLeap = false; 
+
+        public static void Main(string[] args)  
+        {  
+           try{ 
+            Console.WriteLine("Enter Year : ");  
+            int Year = int.Parse(Console.ReadLine());
+
+            if (Year < 1582) Console.WriteLine("It is not possible to calculate leap years earlier that 1582. Please select another Year");  
+            else{
+                IsLeapYear(Year);
+                if (isLeap ==true) Console.WriteLine("{0} is a Leap Year.", Year);  
+                else Console.WriteLine("{0} is not a Leap Year.", Year);  
+            }
+           }catch(System.FormatException){
+               Console.WriteLine("Only intengers are allowed!");
+           }
+        }  
+
+        public static bool IsLeapYear(int year){
+           if (year>=1582 && (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))) return isLeap = true;   
+            else return isLeap = false;   
         }
-    }
+    }  
 }

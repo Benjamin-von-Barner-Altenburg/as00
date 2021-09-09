59de3ea9c41e7a553b34827921506ec3f9fb7c9b
Benjamin von Barner Altenburg <Beal@itu.dk>
2021-09-09 21:51:49 +0200

    implemented 75% error handling.

--- Lecture01.Tests/UnitTest1.cs
+++ Lecture01.Tests/UnitTest1.cs
@@ -8,6 +8,30 @@ namespace Lecture01.Tests
         [Fact]
         public void Test1()
         {
+            Program.Main(new string[0]); 
+
+            Program.IsLeapYear(1200).Equals("It is not possible to calculate leap years earlier that 1582. Please select another Year");
+
+            Program.IsLeapYear(1581).Equals("It is not possible to calculate leap years earlier that 1582. Please select another Year");
+
+            Program.IsLeapYear(2004).Equals(true);
+
+            Program.IsLeapYear(1999).Equals(false);
+
+            Program.IsLeapYear(2020).Equals(true);
+
+            Program.IsLeapYear(2013).Equals(false);
+
+            Program.IsLeapYear(2056).Equals(true);
+
+            Program.IsLeapYear(2055).Equals(false);
+
+            Program.IsLeapYear(2000).Equals(true);
+
+            Program.IsLeapYear(2001).Equals(false);
+
+            Program.IsLeapYear(2013).Equals(false);
+
 
         }
     }
